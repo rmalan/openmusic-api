b61ea56dee3f56757efafaa1804b7ab057a24268
M. Raka Maulana A <rmalan09@outlook.com>
2021-06-26 08:19:02 +0700

    db & schema

---
+++ migrations/1624663098816_create-table-songs.js
@@ -0,0 +1,42 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'varchar(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'varchar(255)',
+      notNull: true,
+    },
+    year: {
+      type: 'integer',
+      notNull: true,
+    },
+    performer: {
+      type: 'varchar(75)',
+      notNull: true,
+    },
+    genre: {
+      type: 'varchar(100)',
+    },
+    duration: {
+      type: 'integer',
+    },
+    inserted_at: {
+      type: 'text',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'text',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
