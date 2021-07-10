90d2b22c34574c026dbb828775f91d9dae34ecf9
M. Raka Maulana A <rmalan09@outlook.com>
2021-07-10 09:12:37 +0700

    Proyek OpenMusic API versi 2

--- src/api/playlists/handler.js
+++ src/api/playlists/handler.js
@@ -52,16 +52,38 @@ class PlaylistsHandler {
     }
   }
 
-  async getPlaylistsHandler(request) {
-    const { id: credentialId } = request.auth.credentials;
-    const playlists = await this._service.getPlaylists(credentialId);
+  async getPlaylistsHandler(request, h) {
+    try {
+      const { id: credentialId } = request.auth.credentials;
+      const playlists = await this._service.getPlaylists(credentialId);
 
-    return {
-      status: 'success',
-      data: {
-        playlists,
-      },
-    };
+      return {
+        status: 'success',
+        data: {
+          playlists,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+
+      response.code(500);
+      console.error(error);
+      return response;
+    }
   }
 
   async deletePlaylistByIdHandler(request, h) {
